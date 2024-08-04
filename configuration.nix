2129fcaf9c42ff1821ce8a985dcd26d5ad0b430d
Iver <iverrog@gmail.com>
2024-08-04 19:03:23 +0200

    first compile with home manager

--- configuration.nix
+++ configuration.nix
@@ -87,8 +87,13 @@
       htop
     ];
   };
-
-  # services.solaar.enable = true;
+  
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "iver" = import ./home.nix;
+    };
+  };
 
   # Install firefox.
   programs.firefox.enable = true;
