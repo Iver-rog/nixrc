0c1b4a47b22a8fdfa99170230ca9e017a0c42cc0
iver <iverrog@gmail.com>
2024-08-27 17:39:01 +0200

    Nvim: added telescope plugin

--- lib-home/nvim.nix
+++ lib-home/nvim.nix
@@ -39,34 +39,65 @@ in
       nvim-treesitter.withAllGrammars
       gruvbox-material
       vim-nix
-      # { # Useful plugin to show you pendig keybinds.
-      #   plugin = which-key-nvim;
-      #   type = "lua";
-      #   config = ''
-      #     require('which-key').setup()
-      #
-      #     -- Document existing key chains
-      #     require('whitch-key').add {
-      #       { '<leader>c, group = '[C]ode'},
-      #       { '<leader>d, group = '[D]ocument'},
-      #       { '<leader>r, group = '[R]rename'},
-      #       { '<leader>s, group = '[S]earch'},
-      #       { '<leader>w, group = '[W]orkspace'},
-      #       { '<leader>t, group = '[T]oggle'},
-      #       { '<leader>h, group = 'Git [H]unk', mode = { 'n', 'v'} },
-      #     }
-      #   '';
-      # }
+      { # Useful plugin to show you pendig keybinds.
+        plugin = which-key-nvim;
+        type = "lua";
+        config = ''
+          require('which-key').setup{}
 
+          -- Document existing key chains
+--          require('whitch-key').add {
+--            { '<leader>c', group = '[C]ode'},
+--            { '<leader>d', group = '[D]ocument'},
+--            { '<leader>r', group = '[R]rename'},
+--            { '<leader>s', group = '[S]earch'},
+--            { '<leader>w', group = '[W]orkspace'},
+--            { '<leader>t', group = '[T]oggle'},
+--            { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v'} },
+--          }
+        '';
+      }
       {
-          # A tree file explore.
+        plugin = telescope-nvim;
+        type = "lua";
+        # dependencies = [
+        #   plenary-nvim
+        #   telescope-fzf-native-nvim
+        #   telescope-ui-select-nvim
+        #   nvim-web-devicons
+        # ];
+        config = ''
+          require('telescope').setup {
+            extensions = {
+              ['ui-select'] = {
+                require('telescope.themes').get_dropdown(),
+              },
+            },
+          }
+        
+          pcall(require('telescope').load_extension, 'fzf')
+          pcall(require('telescope').load_extension, 'ui-select')
+          
+          local builtin = require 'telescope.builtin'
+          vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+          vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+          vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+          vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+          vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+          vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+          vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+          vim.keymap.set('n', '<leader>s.', builtin.grep_string, { desc = '[S]earch [R]resent Files' })
+          vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers'})
+        '';
+      }
+      { # A tree file explore.
           plugin = nvim-tree-lua;
           type = "lua";
           config = ''
           require'nvim-tree'.setup {}
           '';
       }
-      {
+      { # Draws colored bars to indicate git changes
         plugin = gitsigns-nvim;
         type = "lua";
         config = ''
