14dd6ef31394d6ac4f1e92c5fb0c0925a5a0ab2a
iver <iverrog@gmail.com>
2024-09-14 18:11:03 +0200

    nvim: minimal lspconfig test for python

--- lib-home/nvim/lsp.nix
+++ lib-home/nvim/lsp.nix
@@ -3,13 +3,15 @@
   programs.neovim = {
     extraPackages = with pkgs; [
       clang-tools #c/c++
+      pyright
     ];
     plugins = with pkgs.vimPlugins; [
           {
         plugin = nvim-lspconfig;
         type = "lua";
         config = ''
-
+        require'lspconfig'.pyright.setup{}
+   
         '';
       }
     ];
