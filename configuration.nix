ba16622d3f76f3e530ca0a083797558c84774f27
iver <iverrog@gmail.com>
2024-11-09 13:37:22 +0100

    made vscode plugins declarative

--- configuration.nix
+++ configuration.nix
@@ -110,7 +110,6 @@
       blender
       gimp
       darktable
-      vscode
       chromium
       joplin-desktop
       maestral-gui #Dropbox client
@@ -131,6 +130,28 @@
       bat
       entr
       distrobox
+    (vscode-with-extensions.override {
+      vscodeExtensions = with vscode-extensions; [
+        bbenoist.nix
+        vadimcn.vscode-lldb
+        # jnoortheen.nix-ide
+        mkhl.direnv
+        ms-python.python
+        vscodevim.vim
+        rust-lang.rust-analyzer
+        tamasfe.even-better-toml
+        ms-vscode.hexeditor
+        ms-python.debugpy
+        # ms-pyright.pyright
+      # ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      #   {
+      #     name = "remote-ssh-edit";
+      #     publisher = "ms-vscode-remote";
+      #     version = "0.47.2";
+      #     sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+      #   }
+      ];
+    })
     ];
   };
   
