56aa7f622ef22fd9381400e7cdccdea7542972d9
iver <iverrog@gmail.com>
2024-11-14 19:55:39 +0100

    whitespace fixes and attempt at setting up nvim-treesitter also added folding (only works for rust)

--- lib-home/nvim/nvim/init.lua
+++ lib-home/nvim/nvim/init.lua
@@ -77,42 +77,63 @@ require('telescope').setup {
     },
   }
 
-  pcall(require('telescope').load_extension, 'fzf')
-  pcall(require('telescope').load_extension, 'ui-select')
-  
-  local builtin = require 'telescope.builtin'
-  vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-  vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-  vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-  vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-  vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-  vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-  vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-  vim.keymap.set('n', '<leader>s.', builtin.grep_string, { desc = '[S]earch [R]resent Files' })
-  vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers'})
+pcall(require('telescope').load_extension, 'fzf')
+pcall(require('telescope').load_extension, 'ui-select')
+
+local builtin = require 'telescope.builtin'
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>s.', builtin.grep_string, { desc = '[S]earch [R]resent Files' })
+vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers'})
 
 
-  require'nvim-tree'.setup {}
+require'nvim-tree'.setup {}
 
 -- Draws colored bars to indicate git changes
-  require('gitsigns').setup {
-    on_attach = function(bufnr)
-    local gs = package.loaded.gitsigns
-      vim.keymap.set('n', "<leader>gd", gs.reset_hunk)
-      vim.keymap.set('v', "<leader>gd", function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-      vim.keymap.set('n', "<leader>gb", gs.toggle_current_line_blame)
-      vim.keymap.set('n', "]g", function()
-         if vim.wo.diff then return ']c' end
-         vim.schedule(function() gs.next_hunk() end)
-         return '<Ignore>'
-         end, {expr=true})
-       vim.keymap.set('n', "[g", function()
-         if vim.wo.diff then return '[c' end
-         vim.schedule(function() gs.prev_hunk() end)
-         return '<Ignore>'
-         end, {expr=true})
-    end
-    }
+require('gitsigns').setup {
+  on_attach = function(bufnr)
+  local gs = package.loaded.gitsigns
+    vim.keymap.set('n', "<leader>gd", gs.reset_hunk)
+    vim.keymap.set('v', "<leader>gd", function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    vim.keymap.set('n', "<leader>gb", gs.toggle_current_line_blame)
+    vim.keymap.set('n', "]g", function()
+       if vim.wo.diff then return ']c' end
+       vim.schedule(function() gs.next_hunk() end)
+       return '<Ignore>'
+       end, {expr=true})
+     vim.keymap.set('n', "[g", function()
+       if vim.wo.diff then return '[c' end
+       vim.schedule(function() gs.prev_hunk() end)
+       return '<Ignore>'
+       end, {expr=true})
+  end
+}
+
+require('nvim-treesitter').setup{
+  ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'query', 'vim', 'vimdoc', 'python', 'wgsl'},
+  highlight = {
+    enable = true,
+  },
+  indent = {enable = true},
+}
+
+vim.o.foldlevel = 99
+vim.o.foldcolumn = '1'
+vim.wo.foldnestmax = 1
+vim.wo.foldminlines = 1
+vim.o.foldenable = true
+vim.o.foldlevelstart = 99
+vim.o.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').' ... ' . '(' . (v:foldend - v:foldstart + 1) . ' lines)']]
+vim.wo.foldmethod = "syntax"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.cmd('highlight Folded ctermbg=NONE guibg=NONE')
+vim.cmd('highlight FoldColumn ctermfg=NONE guifg=NONE')
 
 require("lsp")
 require("mini-nvim")
