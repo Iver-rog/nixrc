c8ed169e8f73012f07d68f2b4a0922331c56ffd2
iver <iverrog@gmail.com>
2025-03-15 19:31:41 +0100

    Getting rid of warnings

--- configuration.nix
+++ configuration.nix
@@ -54,7 +54,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -121,7 +121,7 @@
       resources
       paraview
       localsend
-      transmission-qt
+      transmission_4-qt
       calibre
       # Terminal programs
       tldr
