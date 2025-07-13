3f16659d17ba36e082e78f07e1df404d758455e1
iver <iverrog@gmail.com>
2025-07-13 02:40:06 +0200

    nvim syntax highlighing (wgsl)

---
+++ lib-home/nvim/nvim/queries/wgsl/highlights.scm
@@ -0,0 +1,20 @@
+(function_declaration) @function
+(struct_declaration) @type
+(variable_declaration) @variable
+(builtin_type) @type.builtin
+(identifier) @variable
+(type_identifier) @type
+(float_literal) @number
+(int_literal) @number
+(bool_literal) @boolean
+(comment) @comment
+[
+  (loop_statement)
+  (if_statement)
+  (else_statement)
+  (for_statement)
+  (continue_statement)
+  (break_statement)
+  (return_statement)
+] @keyword
+
